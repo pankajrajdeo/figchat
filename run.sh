b15ab6b4112413efca7f7045d9df66bdc00dcd27
EC2 Default User <ec2-user@ip-172-31-63-190.ec2.internal>
2025-03-06 14:42:46 +0000

    Added API tokens for additional services

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@
 
 PORT=$1
 [ -z "$PORT" ] && PORT=7860
-[ $PORT -lt 1000 ] && ROOT_PATH_POSTFIX=$PORT && PORT=$((7860+PORT))
+[ $PORT -lt 1000 ] && ROOT_PATH_POSTFIX=$PORT/ && PORT=$((7860+PORT))/
 
 . ../OPENAI_API_KEY.figchat
+. ../GOOGLE_API_KEY.figchat
+. ../CHAINLIT_AUTH_SECRET.figchat
+
 BASE_DATASET_DIR=/reference-data/figchat_datasets/
 PLOT_OUTPUT_DIR=/reference-data/figchat_plots/
 DATASET_INDEX_FILE=/reference-data/figchat_datasets/dataset_index_advanced_paths.tsv
-DATABASE_PATH=/reference-data/figchat_datasets/global_sqlite.db
+DATABASE_URL=postgresql://chainlit:myChain3@chainlit.cbijlsssksnt.us-east-1.rds.amazonaws.com/postgres
 TRAIN_DATA_FILE=/reference-data/figchat_datasets/TRAIN_DATA_FILE.json
 TRAIN_IMAGE_DATA_FILE=/reference-data/figchat_datasets/TRAIN_IMAGE_DATA_FILE.json
 BASE_URL=https://devapp.lungmap.net
@@ -19,12 +22,14 @@ docker run -d --restart unless-stopped -p $PORT:7860  \
 	-v $PLOT_OUTPUT_DIR:$PLOT_OUTPUT_DIR \
         -e BASE_DATASET_DIR="$BASE_DATASET_DIR" \
         -e PLOT_OUTPUT_DIR="$PLOT_OUTPUT_DIR" \
-        -e DATABASE_PATH="$DATABASE_PATH" \
+        -e DATABASE_URL="$DATABASE_URL" \
         -e DATASET_INDEX_FILE="$DATASET_INDEX_FILE" \
         -e TRAIN_DATA_FILE="$TRAIN_DATA_FILE" \
         -e TRAIN_IMAGE_DATA_FILE="$TRAIN_IMAGE_DATA_FILE" \
         -e OPENAI_API_KEY="$OPENAI_API_KEY" \
         -e BASE_URL="$BASE_URL" \
+        -e CHAINLIT_AUTH_SECRET="$CHAINLIT_AUTH_SECRET" \
+        -e GOOGLE_API_KEY="$GOOGLE_API_KEY" \
 	--name figchat \
 	figchat --root-path $ROOT_PATH
 
